097bfaae5c132e0d8705151d603f37f670ec7845
Girish <m.girishroy@gmail.com>
2023-04-29 12:54:32 +0530

    router component creation

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { CssBaseline, createTheme } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <ThemeProvider theme={darkTheme}>
+      <App />
+      <CssBaseline />
+    </ThemeProvider>
+  </BrowserRouter>
+);
