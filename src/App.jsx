f11dcfacb661b8de4329b3113994c0a90735f9fc
Girish <m.girishroy@gmail.com>
2023-04-30 15:48:28 +0530

    loader implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import Container from "@mui/material/Container";
 import RouterComponent from "./components/RouterComponent";
 import NavBar from "./components/NavBar";
+import { Backdrop, CircularProgress } from "@mui/material";
+import { useGlobalContext } from "./store/context";
 
 function App() {
+  const { loading } = useGlobalContext();
   return (
     <>
+      {
+        <Backdrop
+          sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+          open={loading}
+        >
+          <CircularProgress color="inherit" />
+        </Backdrop>
+      }
       <NavBar />
       <Container component={"main"} maxWidth="lg" sx={{ height: "85%" }}>
         <RouterComponent />
