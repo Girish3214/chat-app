b5a21cc80bbcfa30d8221c9a7f70dbd45d90f11d
Girish <m.girishroy@gmail.com>
2023-05-01 17:01:41 +0530

    adding get request

--- src/uitils/serviceCalls.js
+++ src/uitils/serviceCalls.js
@@ -7,8 +7,19 @@ const postRequest = async (api, body) => {
     return response.data;
   } catch (error) {
     console.log(error);
-    return { error: true, msg: error?.response?.data?.msg };
+    return { error: true, msg: error?.response?.data?.msg ?? error?.message };
   }
 };
 
-export { postRequest };
+const getRequest = async (api, body) => {
+  try {
+    const response = await axios.get(api);
+    console.log(response);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    return { error: true, msg: error?.response?.data?.msg ?? error?.message };
+  }
+};
+
+export { postRequest, getRequest };
