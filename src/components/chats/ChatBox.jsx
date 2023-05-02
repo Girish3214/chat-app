ec64b626eacd2dcd455765993f2566c6e7edd0c7
Girish <m.girishroy@gmail.com>
2023-05-02 12:45:50 +0530

    chat-box implementation without input

--- src/components/chats/ChatBox.jsx
+++ src/components/chats/ChatBox.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useGlobalChatContext } from "../../store/chatContext";
 import { useGlobalContext } from "../../store/authContext";
 import useFetchReceiverUser from "../../hooks/useFetchReceiverUser";
+import { Stack } from "@mui/material";
+import moment from "moment";
 
 const ChatBox = () => {
   const { user } = useGlobalContext();
@@ -21,7 +23,29 @@ const ChatBox = () => {
     return <p style={{ textAlign: "center", width: "100%" }}>loading...</p>;
   }
 
-  return <div>ChatBox</div>;
+  return (
+    <Stack spacing={4} className="chat-box">
+      <div className="chat-header">
+        <strong>{receivedUser?.name}</strong>
+      </div>
+      <Stack spacing={3} className="messages">
+        {messages &&
+          messages.map((message, index) => (
+            <Stack
+              key={message.text + index}
+              className={`${
+                message?.senderId === user?._id ? "message self" : "message"
+              }`}
+            >
+              <span>{message.text}</span>
+              <span className="message-footer">
+                {moment(message.createdAt).calendar()}
+              </span>
+            </Stack>
+          ))}
+      </Stack>
+    </Stack>
+  );
 };
 
 export default ChatBox;
